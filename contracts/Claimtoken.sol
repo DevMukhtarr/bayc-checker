d7874822422c5991211e44ce64cf12c0378c1d58
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-18 16:22:34 +0100

    performing sanity checks

--- contracts/Claimtoken.sol
+++ contracts/Claimtoken.sol
@@ -6,9 +6,32 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Claimtoken {
     IERC20 public immutable tokenAddress;
+    bytes32 merkleRoot;
+    address public owner; 
+
+    mapping (address => bool) claimedAirdrop;
+
+    event AirdropClaimed(address holder, uint256 time);
+
+    modifier onlyOwner () {
+        require(msg.sender == owner, "Only owner can perform this action");
+        _;
+    }
+
     constructor(address _tokenAddress, bytes32 _merkleRoot) {
         owner = msg.sender;
         merkleRoot = _merkleRoot;
         tokenAddress = IERC20(_tokenAddress);
     }
+
+    function claimAirdrop (bytes32[] memory _merkleProof, uint256 _amount, string PendingBalanceUpdate) public {
+        require(msg.sender != address(0), "Address zero detected");
+        require(!claimedAirdrop[msg.sender], "Airdrop claimed already");
+
+        bytes32 leaf = keccak256(abi.encodePacked(msg.sender, _amount, PendingBalanceUpdate));
+
+        IERC20(_tokenAddress).transferFrom
+    }
+
+
 }
